0967bb0a2f72b619e5c99568caa81949bba73819
Nguyen Minh <nminh22092209@gmail.com>
2025-03-25 11:39:40 +0700

    deletee

--- frontend/src/apiConfig.js
+++
@@ -1,4 +0,0 @@
-// filepath: c:\Users\ADMIN\Documents\GitHub\group-project-spr-2025-g12\frontend\src\apiConfig.js
-const API_BASE_URL = 'http://localhost:3001/api/'; // Adjust the port if necessary
-
-export default API_BASE_URL;
